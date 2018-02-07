b3ba87393654b1c129e2f1a1a628e1ae7d1ea547
Joseph Ryan <joseph.ryan.iv@gmail.com>
2018-02-07 01:33:24 -0800

    refactored logout functionality and restructured reducers and actions

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -1,5 +1,42 @@
+import { decrementProgress, incrementProgress } from './progress';
+
+// Action Creators
 export const loginAttempt = () => ({ type: 'AUTHENTICATION_LOGIN_ATTEMPT' });
 export const loginFailure = error => ({ type: 'AUTHENTICATION_LOGIN_FAILURE', error });
 export const loginSuccess = json => ({ type: 'AUTHENTICATION_LOGIN_SUCCESS', json });
+export const logoutFailure = error => ({ type: 'AUTHENTICATION_LOGOUT_FAILURE', error });
+export const logoutSuccess = () => ({ type: 'AUTHENTICATION_LOGOUT_SUCCESS' });
 export const sessionCheckFailure = () => ({ type: 'AUTHENTICATION_SESSION_CHECK_FAILURE' });
 export const sessionCheckSuccess = json => ({ type: 'AUTHENTICATION_SESSION_CHECK_SUCCESS', json });
+
+// Log User Out
+export function logUserOut() {
+    return async (dispatch) => {
+        // Turn on spinner
+        dispatch(incrementProgress());
+
+        // Contact the API
+        await fetch(
+            // Where to contact
+            '/api/authentication/logout',
+            // What to send
+            {
+                method: 'GET',
+                credentials: 'same-origin',
+            },
+        )
+            .then((response) => {
+                if (response.status === 200) {
+                    dispatch(logoutSuccess());
+                } else {
+                    dispatch(logoutFailure(`Error: ${response.status}`));
+                }
+            })
+            .catch((error) => {
+                dispatch(logoutFailure(new Error(error)));
+            });
+
+        // Turn off spinner
+        return dispatch(decrementProgress());
+    };
+}
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 const renderLogin = () => <NavLink tag={Link} to="/account/login">Log In</NavLink>;
-const renderGreeting = name => <span>Welcome, {name}</span>;
-
 
 export default class Header extends React.Component {
     constructor(props) {
         super(props);
 
+        this.logOutClick = this.logOutClick.bind(this);
+        this.renderGreeting = this.renderGreeting.bind(this);
         this.toggleNavbar = this.toggleNavbar.bind(this);
         this.state = {
             isOpen: false,
@@ -22,6 +22,20 @@ export default class Header extends React.Component {
         });
     }
 
+    logOutClick(e) {
+        e.preventDefault();
+        const { logUserOutFunction } = this.props;
+        logUserOutFunction();
+    }
+
+    renderGreeting(name) {
+        return (
+            <span>
+                Welcome, {name} | <a href="/logout" onClick={this.logOutClick}>Log Out</a>
+            </span>
+        );
+    }
+
     render() {
         const { isLoggedIn, firstName } = this.props.authentication;
         return (
@@ -32,7 +46,7 @@ export default class Header extends React.Component {
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                                { isLoggedIn ? renderGreeting(firstName) : renderLogin() }
+                                { isLoggedIn ? this.renderGreeting(firstName) : renderLogin() }
                             </NavItem>
                         </Nav>
                     </Collapse>
