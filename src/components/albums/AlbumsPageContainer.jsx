0c320aca5b570f188bb1baa81dfe99f3b8538c33
Joseph Ryan <joseph.ryan.iv@gmail.com>
2018-02-27 00:24:06 -0800

    album backend, saving music data locally

--- src/components/albums/AlbumsPageContainer.jsx
+++ src/components/albums/AlbumsPageContainer.jsx
@@ -6,18 +6,20 @@ import { addAlbum, albumSearchClear, searchAlbums } from '../../actions/albums';
 import AlbumsPage from './AlbumsPage';
 
 export class AlbumsPageContainer extends React.Component {
+
     componentWillUnmount() {
         const { dispatch } = this.props;
         dispatch(albumSearchClear());
     }
 
     render() {
-        const { addAlbumFunction, albums, searchAlbumsFunction } = this.props;
+        const { addAlbumFunction, albums, searchAlbumsFunction, user } = this.props;
         return (
             <AlbumsPage
                 addAlbumFunction={addAlbumFunction}
                 albums={albums}
                 searchAlbumsFunction={searchAlbumsFunction}
+                user={user}
             />
         );
     }
@@ -29,6 +31,6 @@ const mapDispatchToProps = dispatch => bindActionCreators({
     dispatch,
 }, dispatch);
 
-const mapStateToProps = state => ({ albums: state.albums });
+const mapStateToProps = state => ({ albums: state.albums, user: state.user });
 
 export default connect(mapStateToProps, mapDispatchToProps)(AlbumsPageContainer);
